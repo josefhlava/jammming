7a75e34e4b3f9fad8d8db8795ada7f76f173715b
Josef <josefhlava@yahoo.co.uk>
2021-11-30 14:59:01 +0100

    set up passing down the search results and render result list

--- reactapp/src/Components/App/App.js
+++ reactapp/src/Components/App/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [{name: 'name1', artist: 'artist1', album: 'album1', id: 1},
+      {name: 'name2', artist: 'artist2', album: 'album2', id: 2},
+      {name: 'name3', artist: 'artist3', album: 'album3', id: 3}]
+    }
+  }
   render() {
     return (
       <div>
@@ -10,8 +20,8 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            {/* <SearchResults /> */}
-            {/* <Playlist /> */}
+            <SearchResults searchResults={this.state.searchResults}/>
+            <Playlist />
           </div>
         </div>
       </div>
--- reactapp/src/Components/Track/Track.js
+++ reactapp/src/Components/Track/Track.js
@@ -1,9 +1,9 @@
 import React from "react";
 import './Track.css';
 
-export class Track extends React.Component {
-    renderAction(isRemoval) {
-        if (isRemoval) {
+export default class Track extends React.Component {
+    renderAction() {
+        if (this.props.isRemoval) {
             return <buttom className="Track-action">-</buttom>
         } else {
             return <button className="Track-action">+</button>
@@ -14,8 +14,8 @@ export class Track extends React.Component {
         return (
             <div className="Track">
                 <div className="Track-information">
-                    {/* <h3><!-- track name will go here --></h3> */}
-                    {/* <p><!-- track artist will go here--> | <!-- track album will go here --></p> */}
+                    <h3>{this.props.track.name}</h3>
+                    <p>{this.props.track.artist} | {this.props.track.album}</p>
                 </div>
                 {/* <button className="Track-action"><!-- + or - will go here --></button> */}
             </div>
